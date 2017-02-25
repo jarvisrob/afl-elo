f462f3ccac88e982883ecc6eabea663e663d7bf8
Rob Jarvis <jarvisrob@users.noreply.github.com>
2017-02-25 14:02:21 +1100

    cleaned up main elo loop, esp how detects new seasons and does regression and stores ratings for active teams at season start, also commented main elo function better

--- afl_elo.R
+++ afl_elo.R
@@ -65,13 +65,10 @@ RunElo <- function(all.games, team.dictionary, team.data, ground.location, groun
                    param.coeff.travel,
                    param.rating.expansion.init) {
   
-  #print(head(ground.panel.record))
-  
   # Initialise ratings
-  is.founding.team <- (team.data$season.start <= 1897)
-  team.data$rating[is.founding.team] <- rating.mean
-  team.data$rating[!is.founding.team] <- param.rating.expansion.init
-  #print(team.data$rating)
+  yes.founding.team <- (team.data$season.start <= 1897)
+  team.data$rating[yes.founding.team] <- rating.mean
+  team.data$rating[!yes.founding.team] <- param.rating.expansion.init
   
   # Initialise cumulative sum abs error on margin
   margin.cumulative.abs.error <- 0
@@ -79,83 +76,92 @@ RunElo <- function(all.games, team.dictionary, team.data, ground.location, groun
   # Initialise cumulative sum squared error on result, for Brier score calc
   result.cumulative.sq.error <- 0
   
-  # Initialise season and round tracking variables
+  # Initialise season tracking variable
   season.current <- 1897
-  round.current <- 'xx'  # Dummy value to begin, want to trigger logging to values into rating.time.series
-  season.round.current <- paste(season.current, round.current, sep = ' ')
-  
-  for (game.idx in 1:nrow(all.games)) {
-    
-    if (all.games[game.idx, 'round'] != round.current) {
-      round.current <- all.games[game.idx, 'round']
-      #print(round.current)
-      #print(team.data$rating)
-      
-      if (round.current == 'R1') {
-        season.current <- as.integer(format(all.games[game.idx, 'date'], '%Y'))
-        #print(season.current)
-        #print(game.idx)
-        is.regress <- (team.data$season.start < season.current) & (team.data$season.end >= season.current)
-        #if (season.current == 1926) {
-          #print(is.regress)
-          #print(team.data$rating)
-        #}
-        #print(is.regress)
-        if (sum(is.regress) > 0) {
-          team.data$rating[is.regress] <- RegressRating(team.data$rating[is.regress], rating.mean, param.regress.rating)
-          # regress ground rating for each active team
-          #ground.data.col.idx <- c(F, !logical(ncol(ground.data)-1)) & c(F, is.regress)  # Making first col index FALSE to avoid grouond locations col (1st col)
-          #ground.data[, ground.data.col.idx] <- RegressRating(ground.data[, ground.data.col.idx], 0, param.regress.ground)
-        }
-        
-        
-        is.active <- (team.data$season.start <= season.current) & (team.data$season.end >= season.current)
-        # ***** Need to work out how to log all active teams at YYYY start ******
-        rating.time.series[paste(season.current, 'start', sep = ' '), is.active] <- team.data$rating[is.active]
 
-        # Move the Swans to Sydney from 1982 onwards
-        if ((season.current >= 1982) & (team.data['sydney', 'location'] != 'Sydney')) {
-          team.data['sydney', 'location'] <- 'Sydney'
-        }
-        
-        # Start calibration/optimisation on the AFL era
-        if (season.current == 1990) {
-          margin.cumulative.abs.error <- 0
-          result.cumulative.sq.error <- 0
-        }
+  # Main iterative loop: For each game ...
+  for (game.idx in 1:nrow(all.games)) {
+
+    # Update season and round tracking variables
+    season.of.prev.game <- season.current
+    season.current <- all.games[game.idx, "season"]
+    round.current <- all.games[game.idx, "round"]
+    season.round.current <- paste(season.current, round.current, sep = ' ')
+
+    # If this game marks the start of a new season ...
+    if (season.of.prev.game != season.current) {
+      
+      # Ratings regress to mean for teams that played the previous season
+      yes.regress <- (team.data$season.start < season.current) & (team.data$season.end >= season.current)
+      if (any(yes.regress)) {
+        team.data$rating[yes.regress] <- RegressRating(team.data$rating[yes.regress], rating.mean, param.regress.rating)
       }
       
-      season.round.current <- paste(season.current, round.current, sep = ' ')
+      # Determine active teams and record their rating at season start (after regression)
+      yes.active <- (team.data$season.start <= season.current) & (team.data$season.end >= season.current)
+      rating.time.series[paste(season.current, 'start', sep = ' '), yes.active] <- team.data$rating[yes.active]
+    
+      # Move the Swans to Sydney in 1982
+      if ((team.data['sydney', 'location'] != 'Sydney') & (season.current >= 1982)) {
+        team.data['sydney', 'location'] <- 'Sydney'
+      }
+
+      # Start calibration/optimisation on the AFL era
+      if (season.current == 1990) {
+        margin.cumulative.abs.error <- 0
+        result.cumulative.sq.error <- 0
+      }
+    
     }
     
-    
+    # Extract home and away teams, their scores and the ground
     team.home <- team.dictionary[[all.games[game.idx, 'team.home']]]
     team.away <- team.dictionary[[all.games[game.idx, 'team.away']]]
     score.points.home <- all.games[game.idx, 'score.points.home']
     score.points.away <- all.games[game.idx, 'score.points.away']
     ground <- all.games[game.idx, 'ground']
-    
+
+    # Extract team ratings
     rating.home <- team.data[team.home, 'rating']
     rating.away <- team.data[team.away, 'rating']
-      
+
+    # Determine ground adjustments
     rating.ground.adj.home <- CalculateGroundAdj(team.home, ground, season.current, ground.panel.record,
                                                    param.time.window, param.coeff.wins, param.coeff.other)
     rating.ground.adj.away <- CalculateGroundAdj(team.away, ground, season.current, ground.panel.record,
                                                    param.time.window, param.coeff.wins, param.coeff.other)
-    
+
+    # Determine travel adjustments
     rating.travel.adj.home <- CalculateTravelAdj(team.home, ground, team.data, ground.location, travel.distance, param.coeff.travel)
     rating.travel.adj.away <- CalculateTravelAdj(team.away, ground, team.data, ground.location, travel.distance, param.coeff.travel)
-    
+
+    # Determine adjusted ratings = rating + ground adj + travel adj
     rating.home.adj <- rating.home + rating.ground.adj.home + rating.travel.adj.home
     rating.away.adj <- rating.away + rating.ground.adj.away + rating.travel.adj.away
-    
+
+    # Calculate the difference in adj ratings and the expected result and margin
     delta.ratings <- rating.home.adj - rating.away.adj
     result.exp.home <- CalculateResultExp(delta.ratings, param.spread)
     result.exp.away <- 1 - result.exp.home
     margin.exp.home <- CalculateMarginExp(delta.ratings, param.spread, param.margin)
+    
+    # Determine the actual margin and result
+    margin.act.home <- score.points.home - score.points.away
+    result.act.home <- CalculateResultAct(margin.act.home, param.margin)
+    result.act.away <- 1 - result.act.home
 
+    # Calculate the new ratings based on the difference between expected and actual results
+    rating.home.new <- CalculateRatingNew(rating.home, result.exp.home, result.act.home, param.coeff.rating.update)
+    rating.away.new <- CalculateRatingNew(rating.away, result.exp.away, result.act.away, param.coeff.rating.update)
+    
+    # Store the new ratings
+    team.data[team.home, 'rating'] <- rating.home.new
+    team.data[team.away, 'rating'] <- rating.away.new
+    rating.time.series[season.round.current, team.home] <- rating.home.new
+    rating.time.series[season.round.current, team.away] <- rating.away.new
 
-
+    # Update the ground panel records of each team's win/loss/draw at each ground
+    # for each season
     idx.gpr.team.home <- (ground.panel.record$team == team.home) &
                          (ground.panel.record$ground == ground) &
                          (ground.panel.record$season == season.current)
@@ -174,21 +180,6 @@ RunElo <- function(all.games, team.dictionary, team.data, ground.location, groun
       ground.panel.record[idx.gpr.team.home, "draws"] <- ground.panel.record[idx.gpr.team.home, "draws"] + 1
       ground.panel.record[idx.gpr.team.away, "draws"] <- ground.panel.record[idx.gpr.team.away, "draws"] + 1
     }
-    
-    
-    
-    margin.act.home <- score.points.home - score.points.away
-    result.act.home <- CalculateResultAct(margin.act.home, param.margin)
-    result.act.away <- 1 - result.act.home
-    
-    rating.home.new <- CalculateRatingNew(rating.home, result.exp.home, result.act.home, param.coeff.rating.update)
-    rating.away.new <- CalculateRatingNew(rating.away, result.exp.away, result.act.away, param.coeff.rating.update)
-    
-    team.data[team.home, 'rating'] <- rating.home.new
-    team.data[team.away, 'rating'] <- rating.away.new
-    
-    rating.time.series[season.round.current, team.home] <- rating.home.new
-    rating.time.series[season.round.current, team.away] <- rating.away.new
     
     #rating.ground.adj.home.new <- CalculateRatingNew(rating.ground.adj.home, result.exp.home, result.act.home, param.coeff.ground.update)
     #rating.ground.adj.away.new <- CalculateRatingNew(rating.ground.adj.away, result.exp.away, result.act.away, param.coeff.ground.update)
@@ -210,22 +201,26 @@ RunElo <- function(all.games, team.dictionary, team.data, ground.location, groun
     # all.games.elo[game.idx, "rating.travel.adj.away.before"]
     # all.games.elo[game.idx, "rating.away.adj.before"]
 
+    # Store the details of the Elo variables for this game for post-analysis
+    all.games.elo[game.idx, 1:2] <- c(team.home, team.away)
+    all.games.elo[game.idx, 3:ncol(all.games.elo)] <- c(rating.home, rating.ground.adj.home, rating.travel.adj.home, rating.home.adj,
+                                                        rating.away, rating.ground.adj.away, rating.travel.adj.away, rating.away.adj,
+                                                        delta.ratings,
+                                                        result.exp.home, result.exp.away, margin.exp.home,
+                                                        result.act.home, result.act.away, margin.act.home,
+                                                        result.act.home - result.exp.home, result.act.away - result.exp.away, margin.act.home - margin.exp.home,
+                                                        rating.home.new - rating.home, rating.home.new, rating.away.new)
 
-     all.games.elo[game.idx, 1:2] <- c(team.home, team.away)
-     all.games.elo[game.idx, 3:ncol(all.games.elo)] <- c(rating.home, rating.ground.adj.home, rating.travel.adj.home, rating.home.adj,
-                                                         rating.away, rating.ground.adj.away, rating.travel.adj.away, rating.away.adj,
-                                                         delta.ratings,
-                                                         result.exp.home, result.exp.away, margin.exp.home,
-                                                         result.act.home, result.act.away, margin.act.home,
-                                                         result.act.home - result.exp.home, result.act.away - result.exp.away, margin.act.home - margin.exp.home,
-                                                         rating.home.new - rating.home, rating.home.new, rating.away.new)
-    
+    # Update the cumulative error tracking variables
     margin.cumulative.abs.error <- margin.cumulative.abs.error + abs(margin.act.home - margin.exp.home)
     result.cumulative.sq.error <- result.cumulative.sq.error + (result.exp.home - result.act.home)^2
+  
   }
-  
-  team.data$is.active <- is.active
-  
+
+  # Record the currently active teams at the conlcusion of all games iterated
+  team.data$yes.active <- yes.active
+
+  # Collate and return the results
   elo.result <- list(team.data, rating.time.series, ground.panel.record, all.games.elo, 
                      margin.cumulative.abs.error, result.cumulative.sq.error)
   elo.result
