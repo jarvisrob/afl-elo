00533673c93469cab4c3ab75a472fb5cfacbb926
Rob Jarvis <jarvisrob@users.noreply.github.com>
2017-09-28 14:53:45 +1000

    scraper now ignores byes and creates a data frame for the fixture as output of the function, also added 'Greater Western Sydney' to the team dict as that is how it appears on the season results/fixture page of AFL Tables

--- afltables_scrape_season_fixture.R
+++ afltables_scrape_season_fixture.R
@@ -12,33 +12,40 @@ ScrapeAFLTablesSeasonFixture <- function(season) {
   
   relevant.html.tables <- soup %>% html_nodes("body center > table")
   
-  relevant.html.tables %>% .[[2*24-1]] %>% html_nodes("b:nth-of-type(1)") %>% html_text() %>% .[1]
-  relevant.html.tables %>% .[[2*23]] %>% html_nodes("table") %>% .[[1]] %>% html_nodes("a") %>% html_text()
+  fixture <- data.frame(round = integer(), game = integer(), team.home = character(), team.away = character(), ground = character())
   
-  loop.done <- FALSE
   rnd <- 1
   repeat {
+    
     rnd.str <- relevant.html.tables %>% .[[2*rnd-1]] %>% html_nodes("b:nth-of-type(1)") %>% html_text() %>% .[1]
     if (rnd.str %>% pmatch("Round", .) %>% is.na()) {
       break
     }
-    print(rnd.str)
+    # print(rnd.str)
+    
     game <- 1
     repeat {
       game.str.raw <- relevant.html.tables %>% .[[2*rnd]] %>% html_nodes("table") %>% .[[game]] %>% html_nodes("a") %>% html_text()
       if (game.str.raw %>% is_empty()) {
         break
       }
-      home.team <- game.str.raw[1]
-      ground <- game.str.raw[2]
-      away.team <- game.str.raw[3]
-      print(paste0(home.team, " v ", away.team, " at ", ground))
+      if (length(game.str.raw) > 1) {
+        team.home <- game.str.raw[1]
+        ground <- game.str.raw[2]
+        team.away <- game.str.raw[3]
+        # print(paste0(team.home, " v ", team.away, " at ", ground))
+        
+        game.row <- data.frame(round = rnd, game = game, team.home = team.home, team.away = team.away, ground = ground)
+        # str(game.row)
+        fixture <- rbind(fixture, game.row)
+      }
       game <- game + 1
     }
+    
     rnd <- rnd + 1
   }
   
-  
+  fixture
 }
 
 
